fcd044ed91837a4a5b13fbabdacdde38662db404
Training <Training@192.168.253.9>
2020-03-11 15:44:55 +0530

    new one

--- JunitDemo/src/Calculator.java
+++ JunitDemo/src/Calculator.java
@@ -15,4 +15,9 @@ public class Calculator {
 		}
 		return a/b;
 	}
+	public static void main(String[] args) {
+		Calculator c=new Calculator();
+		System.out.println("addition of double (2+6) :"+c.add(2,6));
+		System.out.println("subtract of double (6-2) :"+c.subtract(6, 2));
+	}
 }
